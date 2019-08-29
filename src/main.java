ef8aae32a8369f5f5dcfc722d30d97239d141070
Armando Valdez <avaldez@zagmail.gonzaga.edu>
2019-08-29 10:12:47 -0700

    Inserted a thrid print line

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args){
         System.out.println("My second Hello World!");
         System.out.println("Coding is fun");
+        System.out.println("New add for github");
     }
 }
